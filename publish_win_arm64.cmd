4f1e31847202d2aa86d46408df06bcdd04cccb6f
Gustave Monce <gustave.monce@outlook.com>
2024-09-08 00:01:55 +0200

    Remove tocbsvhdx:publish from ci for now

--- publish_win_arm64.cmd
+++ publish_win_arm64.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-msbuild /m /t:restore,tocbs:publish,tocbsffunopool:publish,tocbsvhdx:publish,tocbsvhdxnopool:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir="%CD%\publish\artifacts\win-arm64\CLI" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToCBS\ToCBS.sln
+msbuild /m /t:restore,tocbs:publish,tocbsffunopool:publish,tocbsvhdxnopool:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir="%CD%\publish\artifacts\win-arm64\CLI" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToCBS\ToCBS.sln
