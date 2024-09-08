0ed5fc8b671b09d611a67739ac891ef423d56fc0
Gustave Monce <gustave.monce@outlook.com>
2024-09-08 12:01:37 +0200

    Create a single solution

--- publish_win_arm64.cmd
+++ publish_win_arm64.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-msbuild /m /t:restore,tocbs:publish,tocbsffunopool:publish,tocbsvhdxnopool:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir="%CD%\publish\artifacts\win-arm64\ToCBS" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToCBS\ToCBS.sln
-
-msbuild /m /t:restore,tospkg:publish,tospkgffu:publish,tospkgvhdx:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir="%CD%\publish\artifacts\win-arm64\ToSPKG" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToSPKG\ToSPKG.sln
+msbuild /m /t:restore,tospkg:publish,tospkgffu:publish,tospkgvhdx:publish,tocbs:publish,tocbsffunopool:publish,tocbsvhdxnopool:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir="%CD%\publish\artifacts\win-arm64\CLI" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release MobilePackageGen.sln
--- publish_win_x64.cmd
+++ publish_win_x64.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-msbuild /m /t:restore,tocbs:publish,tocbsffunopool:publish,tocbsvhdxnopool:publish /p:Platform=x64 /p:RuntimeIdentifier=win-x64 /p:PublishDir="%CD%\publish\artifacts\win-x64\ToCBS" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToCBS\ToCBS.sln
-
-msbuild /m /t:restore,tospkg:publish,tospkgffu:publish,tospkgvhdx:publish /p:Platform=x64 /p:RuntimeIdentifier=win-x64 /p:PublishDir="%CD%\publish\artifacts\win-x64\ToSPKG" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release ToSPKG\ToSPKG.sln
+msbuild /m /t:restore,tospkg:publish,tospkgffu:publish,tospkgvhdx:publish,tocbs:publish,tocbsffunopool:publish,tocbsvhdxnopool:publish /p:Platform=x64 /p:RuntimeIdentifier=win-x64 /p:PublishDir="%CD%\publish\artifacts\win-x64\CLI" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release MobilePackageGen.sln
