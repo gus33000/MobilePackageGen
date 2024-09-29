c013ddefff2a90fa0a4d1d7aaaba6b884ff9f4c9
Gustave Monce <gustave.monce@outlook.com>
2024-09-29 18:12:57 +0200

    Dont use single file publish
    This creates issues with the 7z dlls

--- publish_win_x64.cmd
+++ publish_win_x64.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-msbuild /m /t:restore,cbstoreg:publish,secwim2wim:publish,mobilepackagegen:publish /p:Platform=x64 /p:RuntimeIdentifier=win-x64 /p:PublishDir="%CD%\publish\artifacts\win-x64" /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release /p:IncludeNativeLibrariesForSelfExtract=true MobilePackageGen.sln
+msbuild /m /t:restore,cbstoreg:publish,secwim2wim:publish,mobilepackagegen:publish /p:Platform=x64 /p:RuntimeIdentifier=win-x64 /p:PublishDir="%CD%\publish\artifacts\win-x64" /p:PublishTrimmed=false /p:Configuration=Release /p:IncludeNativeLibrariesForSelfExtract=true MobilePackageGen.sln
 
 mkdir %CD%\publish\artifacts\win-x64-symbols
 move %CD%\publish\artifacts\win-x64\*.pdb %CD%\publish\artifacts\win-x64-symbols\
