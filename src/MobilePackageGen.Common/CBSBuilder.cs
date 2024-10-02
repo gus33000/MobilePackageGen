a895b609bb98daba135ebbb114c17fb5030b7052
BlueRain-debug <68976789+BlueRain-debug@users.noreply.github.com>
2024-10-02 15:21:57 +0800

    Update CBSBuilder.cs: fix typo

--- src/MobilePackageGen.Common/CBSBuilder.cs
+++ src/MobilePackageGen.Common/CBSBuilder.cs
@@ -66,8 +66,8 @@ namespace MobilePackageGen
                                                         .Replace("$(runtime.system32)", @"windows\system32")
                                                         .Replace("$(runtime.wbem)", @"windows\system32\wbem")
                                                         .Replace("$(runtime.drivers)", @"windows\system32\drivers")
-                                                        .Replace("$(runtime.programfiles)", "Program Files");
-                                                        .Replace("$(runtime.programdata)", "ProgramData");
+                                                        .Replace("$(runtime.programfiles)", "Program Files")
+                                                        .Replace("$(runtime.programdata)", "ProgramData")
                                                         .Replace("$(runtime.startmenu)", @"ProgramData\Microsoft\Windows\Start Menu");
 
                     if (cbs.AssemblyIdentity.ProcessorArchitecture.Equals("arm64.arm"))
